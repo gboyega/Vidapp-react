b118b9c0e8f458c418bd2921f0f41e1afad9fc51
gboyega <jaiyeolagboyega@gmail.com>
2019-12-24 15:40:15 +0100

    added app name

--- src/VidPlayer.js
+++ src/VidPlayer.js
@@ -1,15 +1,26 @@
 import React, {Component} from 'react';
 import Suggestion from "./Suggestion";
-import { Form, Container, Embed, Divider, Grid, Icon } from "semantic-ui-react";
+import {
+  Form,
+  Container,
+  Embed,
+  Divider,
+  Grid,
+  Icon,
+  Header,
+} from "semantic-ui-react";
 
 class VidPlayer extends Component{
   constructor(){
     super();
     this.state = {
       query: 'space',
-      url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&key=AIzaSyDYfC8I1ZFiWh6DWMUybfKM_k17ZVc4NqQ&q=' ,
+      url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&key=AIzaSyDYfC8I1ZFiWh6DWMUybfKM_k17ZVc4NqQ&q=',
       mainVideo: '',
-      suggestedVideos: []
+      mainVideoImage: '',
+      mainVideoTitle: '',
+      suggestedVideos: [],
+      moreSuggestions: []
     };
     this.getVideos=this.getVideos.bind(this);
     this.searchVideos=this.searchVideos.bind(this);
@@ -27,8 +38,15 @@ class VidPlayer extends Component{
       fetch(url).then(response => response.json())
       .then(data => {
          let firstVideo = data.items.shift();
-         this.setState({...this.state, mainVideo:firstVideo.id.videoId, suggestedVideos:data.items});
-        
+         let suggestions = data.items.splice(0, 9);
+         this.setState({
+           ...this.state,
+           mainVideo: firstVideo.id.videoId,
+           mainVideoTitle: firstVideo.snippet.title,
+           suggestedVideos: suggestions,
+           moreSuggestions: data.items,
+           mainVideoImage: firstVideo.snippet.thumbnails.high.url
+         });
       })
     )
   }
@@ -43,14 +61,21 @@ class VidPlayer extends Component{
   }
 
   changeVideo(video){
-    this.setState({...this.state, mainVideo: video.id.videoId});
+    this.setState({
+      ...this.state,
+      mainVideo: video.id.videoId,
+      mainVideoImage: video.snippet.thumbnails.high.url,
+      mainVideoTitle: video.snippet.title
+    });
   }
 
   render(){
-    console.log(this.state.mainVideo, process.env.REACT_APP_API_KEY);
+    console.log(this.state.mainVideoImage);
     return (
       <div>
-        <p></p>
+        <Header as="h1" textAlign="left" color="red" style={{margin:"10px"}}>
+          Vidapp
+        </Header>
         <Container textAlign="center">
           <Form onSubmit={this.searchVideos} size="large">
             <Form.Group>
@@ -65,13 +90,37 @@ class VidPlayer extends Component{
             </Form.Group>
           </Form>
 
-          <Embed id={this.state.mainVideo} placeholder="" source="youtube" />
+          <Header size="large" textAlign="left">
+            {this.state.mainVideoTitle}
+          </Header>
+
+          <Embed
+            id={this.state.mainVideo}
+            placeholder={this.state.mainVideoImage}
+            source="youtube"
+          />
 
           <Divider horizontal>Suggestions</Divider>
 
           <Grid doubling columns={3}>
             {this.state.suggestedVideos.map((video, i) => (
-              <Suggestion video={video} changeVideo={this.changeVideo} key={i} />
+              <Suggestion
+                video={video}
+                changeVideo={this.changeVideo}
+                key={i}
+              />
+            ))}
+          </Grid>
+
+          <Divider horizontal> More Suggestions</Divider>
+
+          <Grid doubling columns={5}>
+            {this.state.moreSuggestions.map((video, i) => (
+              <Suggestion
+                video={video}
+                changeVideo={this.changeVideo}
+                key={i}
+              />
             ))}
           </Grid>
         </Container>
